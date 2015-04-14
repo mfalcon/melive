f706bea6b826f5a69ee8b6a959b57333a693e423
Mariano Falcon <mf2286@gmail.com>
2015-04-13 22:44:22 -0300

    initial commit, adding existing files

---
+++ meliredis.py
@@ -0,0 +1,203 @@
+#!/home/mf/.virtualenvs/meli/bin/python
+# -*- coding: utf-8 -*-
+
+import importlib
+import json
+
+from bson import json_util
+import os
+import sys
+import time
+import timeit
+import logging
+import math
+import multiprocessing
+from datetime import datetime
+from random import shuffle, randint
+
+from dateutil import relativedelta
+import redis 
+
+import meli_api
+
+
+INITIAL_OFFSET = 0
+RES_LIMIT = 200
+IDS_LIMIT = 50
+PAGE_LIMIT = 1 #10 #total pages to scrap
+BULK_ITEMS = 300 #get bulk items information to decrease api calls
+WORKERS_NUM = 10
+
+
+ALLOWED_CATEGORIES = {
+    'MLA352542': 'iPhone 6 16gb',
+    'MLA352543': 'iPhone 6 64gb',
+    'MLA352546': 'iPhone 6 128gb',
+    'MLA119876': 'Samsung Galaxy 4',
+    'MLA127623': 'Samsung Galaxy 5',
+    'MLA351978': 'Moto G',
+    'MLA126252': 'Xperia Z2',
+    'MLA372245': 'Xperia',
+}
+
+
+rd = redis.StrictRedis(host='localhost', port=6379, db=1)
+p = rd.pubsub()
+
+def get_datetime(utc_diff=3): #argentina utc -3 
+    return datetime.utcnow() - relativedelta(hours=utc_diff)
+
+def extract_datetime(item_date):
+    #eg '2015-03-03T23:18:11.000Z'
+    #TODO: handle timezones
+    return datetime.strptime(item_date, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+
+def get_rdid(item_type, item_id):
+    return item_type + '-' + str(item_id)
+
+
+
+class MeliCollector(): #make all into a class  
+    def __init__(self):
+        api_module = importlib.import_module('meli_api')
+        self.mapi = getattr(api_module, 'MeliAPI')()
+        self.sid = 'MLA'
+       
+    
+    def started_today(self, item_date):
+        diff = datetime.today() - extract_datetime(item_date)
+        if diff.days == 0:
+            return True
+        return False
+        
+    def get_stats(self):
+        stats = {}
+        for category_id in ALLOWED_CATEGORIES:
+            sold_quantity = int(eval(rd.get(get_rdid('categories', category_id))))
+            stats[category_id] = [ALLOWED_CATEGORIES[category_id], sold_quantity]
+        
+        rd.publish('categories', json.dumps(stats))
+        return stats
+    
+    def update_category(self, category_id, item_sold_today):
+        redis_id = get_rdid('categories', category_id)
+        in_redis = rd.get(redis_id)
+        if not in_redis:
+            rd.set(redis_id, item_sold_today)   
+        else:
+            sold_acum = int(eval(in_redis)) + item_sold_today
+            rd.set(redis_id, sold_acum)
+        
+       
+
+    def insert_item(self, item, pn):
+        redis_id = get_rdid('items', item['id'])
+        in_redis = rd.get(redis_id)
+        if not in_redis or in_redis == 'null': #TODO: ??
+            #first time considering the item today
+            #check if the item started selling today
+            if self.started_today(item['start_time']):
+                sold_today = item['sold_quantity']
+                rd.set(redis_id, {'prev_sold_quantity': 0, 'sold_today':sold_today}) #TODO: add another fields
+            else:
+                #theres a flaw here, if the item didnt started selling today and its the first time the item appears
+                sold_today = 0
+                rd.set(redis_id, {'prev_sold_quantity':item['sold_quantity'],'sold_today':sold_today})
+            
+        else: #item already in redis, add sold_quantity diff
+            prev_sold = eval(in_redis)['prev_sold_quantity']
+            sold_today = item['sold_quantity'] - prev_sold
+            rd.set(redis_id, {'prev_sold_quantity':prev_sold,'sold_today':sold_today})
+        
+        self.update_category(item['category_id'], sold_today)
+    
+    
+    def cats_collector(self, queue):
+        print os.getpid(),"working"
+        while True:
+            catid = queue.get(True)
+            print os.getpid(), "got", catid
+            print "getting items"
+            self.get_items(catid)
+            if catid == 'sentinel':
+                print "breaking"
+                break
+            
+            
+
+    def get_items(self, cat_id, limit=RES_LIMIT):
+        """
+        get all the items of a category.
+        The history of an item can be done matching records with the
+        same id.
+        """
+        offset = 0
+        data = []
+        start_time = timeit.timeit()
+        items_data = self.mapi.search_by_category(cat_id, limit, offset)
+        total_pages = items_data['paging']['total']/items_data['paging']['limit'] #FIXME: RES_LIMIT not paging limit
+        if total_pages > PAGE_LIMIT:
+            total_pages = PAGE_LIMIT
+        total_results = total_pages * limit
+        i = 0
+        for pn in range(total_pages):
+            print pn
+            items_data = self.mapi.search_by_category(cat_id, limit, offset)
+            offset += int(limit)
+                           
+            items_ids = [item['id'] for item in items_data['results']]
+            total_ids = len(items_ids)
+            total_calls = total_ids / IDS_LIMIT
+            
+            items = []
+            for j in range(total_calls):
+                sub_items = self.mapi.get_items_data(items_ids[IDS_LIMIT*j:IDS_LIMIT*j+IDS_LIMIT])
+                if sub_items:
+                    items += sub_items
+            
+            to_insert_items = items
+            to_insert_hist = []
+            for item in items:
+                self.insert_item(item, pn)
+
+        return data
+    
+
+    def collect_categories(self, cat_ids):
+            for catid in cat_ids:
+                print catid
+                self.get_items(catid)
+            import pdb; pdb.set_trace()
+
+
+def main(workers=WORKERS_NUM):
+    jobs = []
+    mc = MeliCollector()
+
+    if workers != 1:
+        while True:
+            procs = []
+            cats = ALLOWED_CATEGORIES.keys() #FIXME: send all cats to get_all_cats
+            cats_q = multiprocessing.Queue()
+            [cats_q.put(cat) for cat in cats]
+            [cats_q.put('sentinel') for i in range(workers)]
+            the_pool = multiprocessing.Pool(workers, mc.cats_collector,(cats_q,))
+            the_pool.close()
+            the_pool.join()
+
+            print "batch finished"
+            stats = mc.get_stats()
+            print stats
+            
+
+    else:
+        mc.collect_categories(['MLA1051'])
+
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'test':
+        main(workers=1)
+    else:
+        main(workers=multiprocessing.cpu_count() * 2)
