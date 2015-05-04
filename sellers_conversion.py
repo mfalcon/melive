62fe393c79d8aff3835c8c6d179fbd91b91fe94c
Mariano Falcon <mf2286@gmail.com>
2015-05-04 18:05:53 -0300

    adapting code to use redis hashes structure

--- sellers_conversion.py
+++ sellers_conversion.py
@@ -53,37 +53,21 @@ class MeliCollector(): #make all into a class
         self.sid = 'MLA'
         today = datetime.today()
         self.today = datetime.isoformat(datetime(today.year,today.month,today.day))
-        self.items = []
+      
 
-
-    def publish_stats(self):
-        stats = {}
-        for item_id, item_title in self.items:
-            item_data = rd.get(item_id)
-            data = eval(item_data)
-            sold_quantity = data['sold_quantity']
-            today_visits = data['today_visits']
-            conversion_rate = today_visits/sold_quantity
-            stats[item_id] = [item_title, sold_quantity, today_visits, conversion_rate]
-            
-        
-        rd.publish('sales', json.dumps(stats))
-        #rd.publish('visits', json.dumps(stats))
-        return stats
-       
-
-    def insert_item(self, item, seller_id):
+    def insert_item(self, item, seller_id, time_point):
         sold_today = 0
         sold_diff = 0
         
-        in_redis = rd.get(item['id'])
+        #in_redis = rd.get(item['id'])
+        in_redis = rd.hmget('sellers-%s' % seller_id, item['id'])
+        
         if not in_redis: # or in_redis == 'null': #TODO: ??
             #first time considering the item today
             prev_sold = item['sold_quantity']
-            self.items.append((item['id'], item['title']))
             
         else: #item already in redis, add sold_quantity diff
-            item_redis = eval(in_redis)
+            item_redis = json.loads(in_redis)
             prev_sold = item_redis['prev_sold_quantity']
             #FIXME: sometimes I get an -1 value
             sold_diff = item['sold_quantity'] - (item_redis['sold_today'] + prev_sold)
@@ -91,31 +75,36 @@ class MeliCollector(): #make all into a class
 
         
         #meli datetime example: 2015-04-30T20:00:00.000-03:00
-        today_visits = self.mapi.get_items_visits([item['id']], self.today, datetime.isoformat(datetime.now()))
+        today_visits = self.mapi.get_items_visits([item['id']], self.today, time_point)
                     
         item_data = {
                 'prev_sold_quantity': prev_sold,
                 'sold_today': sold_today,
                 'sold_diff': sold_diff,
                 'today_visits': today_visits,
+                'conversion-rate': today_visits/sold_today,
+                'title': item['title']
         }
-        rd.set(item['id'], item_data)
         
+        #rd.set(item['id'], item_data)
+        rd.hmset('sellers-%s' % seller_id, {'items-%s' % item['id']: item_data})
+        rd.publish('sellers-%s' % seller_id)
         
-    def cats_collector(self, queue):
+    def sellers_collector(self, queue):
         print os.getpid(),"working"
         while True:
-            catid = queue.get(True)
-            print os.getpid(), "got", catid
+            seller_id = queue.get(True)
+            print os.getpid(), "got", seller_id
             print "getting items"
-            self.get_items(catid)
+            time_point = datetime.isoformat(datetime.now()) #uniform datetime
+            self.get_items(seller_id, time_point)
             if catid == 'sentinel':
                 print "breaking"
                 break
 
 
 
-    def get_items(self, seller_id, limit=RES_LIMIT):
+    def get_items(self, seller_id, time_point, limit=RES_LIMIT):
         """
         get all the items of a seller.
         """
@@ -133,16 +122,18 @@ class MeliCollector(): #make all into a class
             if items:
                 #separate into chunks and make a call to self.mapi.get_items_visits(ids_list, date_from, date_to)
                 for item in items:
-                    self.insert_item(item, seller_id) 
+                    self.insert_item(item, seller_id, time_point) 
+        
+        
 
     
 
     def collect_sellers(self, sellers_id):
             for seller_id in sellers_id:
                 self.get_items(seller_id)
-            print "publishing stats"
-            stats = self.publish_stats()
-            print stats
+                stats = self.publish_stats()
+                print "publishing stats of seller %s" % seller_id
+                print stats
                 
 
 
@@ -152,16 +143,16 @@ def main(workers):
 
     if workers != 1:
         sellers = SELLERS.keys()
-        for cat_id in cats:
-            print "setting category: %s" % cat_id
-            rd.set(cat_id, {'total_sold': 0})
+        for seller in sellers:
+            print "setting seller: %s" % seller
+            rd.set('seller:%s' % seller, json.dumps({}))
             
         while True:
             procs = []
             cats_q = multiprocessing.Queue()
             [cats_q.put(cat) for cat in cats]
             [cats_q.put('sentinel') for i in range(workers)]
-            the_pool = multiprocessing.Pool(workers, mc.cats_collector,(cats_q,))
+            the_pool = multiprocessing.Pool(workers, mc.sellers_collector,(cats_q,))
             the_pool.close()
             the_pool.join()
 
